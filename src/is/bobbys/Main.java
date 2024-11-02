58513e710ec95e86f1103848ceeaa2aabe583f64
HerenYU <971054924yu@gmail.com>
2024-11-02 23:10:20 +0800

    Update Main.java
    添加了强制置顶

--- src/is/bobbys/Main.java
+++ src/is/bobbys/Main.java
@@ -2,56 +2,39 @@ package is.bobbys;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public final class Main {
-    private static Font font;
-
+public class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperties());
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        if (args.length == 1) {
-            if (args[0].equals("setting")) {
-                putMiddle(new ToolFrame());
+
+        JFrame frame = new JFrame("随机点名");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 200);
+        frame.setLayout(new BorderLayout());
+        frame.setResizable(false); // 设置窗口不可调整大小
+
+        JCheckBox alwaysOnTopCheckBox = new JCheckBox("强制置顶(用于PPT播放期间)");
+        alwaysOnTopCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    frame.setAlwaysOnTop(true);
+                } else {
+                    frame.setAlwaysOnTop(false);
+                }
             }
-        } else {
-            loadFont();
-            JFrame frame = new JFrame("随机点名");
-            frame.setResizable(false);
-            frame.setSize(350, 200);
-            putMiddle(frame);
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        });
 
-            frame.getContentPane().add(new Button(font), BorderLayout.CENTER);
-            frame.setVisible(true);
-        }
-    }
+        Button button = new Button(new Font("微软雅黑", Font.PLAIN, 60));
+        frame.add(button, BorderLayout.CENTER);
+        frame.add(alwaysOnTopCheckBox, BorderLayout.SOUTH);
 
-    public static void putMiddle(Container container) {
-        Rectangle r = new Rectangle();
-        r.x = Toolkit.getDefaultToolkit().getScreenSize().width / 2 - container.getWidth() / 2;
-        r.y = Toolkit.getDefaultToolkit().getScreenSize().height / 2 - container.getHeight() / 2;
-        r.width = container.getWidth();
-        r.height = container.getHeight();
-        container.setBounds(r);
-    }
-
-    private static void loadFont() {
-        File file = new File("font.ttf");
-        if (file.exists()) {
-            try {
-                font = Font.createFont(Font.TRUETYPE_FONT, file).deriveFont(60.0f);
-            } catch (FontFormatException | IOException e) {
-                e.printStackTrace();
-                font = new Font("微软雅黑", Font.PLAIN, 60);
-            }
-        } else {
-            font = new Font("微软雅黑", Font.PLAIN, 60);
-        }
+        frame.setVisible(true);
     }
 }
